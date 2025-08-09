1dd322920d57137397abd2619bc626334742f45e
gracism <gracism@example.com>
2025-08-09 00:57:31 -0700

    Dynamically update the thought of the day.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 mkdir -p output
 cp -R -p images output
 cp -R -p css output
+cp -R -p data output
 TEMPLATES='-p templates/google.mustache -p templates/t2.mustache -p templates/head.mustache -p templates/foot.mustache'
 npx mustache ${TEMPLATES} data/thoughts.json templates/thoughts.mustache > output/thoughts.html
 npx mustache ${TEMPLATES} data/index.json templates/index.mustache > output/index.html
