aa5466a80776ba3e51674ee6d4566f655f6fde20
gracism <gracism@example.com>
2025-08-08 20:30:53 -0700

    New version of website.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p output
 cp -R -p images output
 cp -R -p css output
 TEMPLATES='-p templates/google.mustache -p templates/t2.mustache -p templates/head.mustache -p templates/foot.mustache'
-mustache ${TEMPLATES} data/thoughts.json templates/thoughts.mustache > output/thoughts.html
-mustache ${TEMPLATES} data/index.json templates/index.mustache > output/index.html
-mustache ${TEMPLATES} data/music.json templates/music.mustache > output/music.html
-mustache ${TEMPLATES} data/things.json templates/things.mustache > output/things.html
+npx mustache ${TEMPLATES} data/thoughts.json templates/thoughts.mustache > output/thoughts.html
+npx mustache ${TEMPLATES} data/index.json templates/index.mustache > output/index.html
+npx mustache ${TEMPLATES} data/music.json templates/music.mustache > output/music.html
+npx mustache ${TEMPLATES} data/things.json templates/things.mustache > output/things.html
